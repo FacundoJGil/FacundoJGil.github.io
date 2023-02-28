2486b2d171ebf6ea356c03962a764558a63e7112
Facuras <31223936+Facuras@users.noreply.github.com>
2023-02-28 01:21:55 -0300

    update

--- CV.js
+++ CV.js
@@ -7,7 +7,24 @@ async function foo() {
     foo().then(data => {
     document.getElementById("nombre").innerHTML=data.datos_personales.nombre +" "+ data.datos_personales.apellido;
   });
+    foo().then(data => {
+    document.getElementById("nacimiento").innerHTML=data.datos_personales.nacimiento;
+  });
+
+    foo().then(data => {
+    document.getElementById("nacimiento").insertAdjacentHTML('beforeend',`<p>D.N.I ${data.datos_personales.documento}</p>`);}
+    );
+    foo().then(data=>{
+        document.getElementById("nacimiento").insertAdjacentHTML('beforeend',`<p>${data.datos_personales.calle}, ${data.datos_personales.localidad}, ${data.datos_personales.provincia}, ${data.datos_personales.pais}</p>`);}
+    );
+    foo().then(data => {
+        document.getElementById("nacimiento").insertAdjacentHTML('beforeend',`<p>${data.datos_personales.telefono}</p>`);}
+        );
+    foo().then(data => {
+        document.getElementById("nacimiento").insertAdjacentHTML('beforeend',`<p>${data.datos_personales.mail}</p>`);}
+            );
  
+
 
 /*var info;
 
